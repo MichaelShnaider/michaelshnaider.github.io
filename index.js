a05f16e672703b1635acca24478e2f5290985f99
mishnaid <mishnaid@edu.uwaterloo.ca>
2017-09-26 14:37:15 -0400

    helpful info

--- index.js
+++ index.js
@@ -76,3 +76,22 @@ document.getElementById("yelpCampSmall").addEventListener("click",function(){
         currentlyActive="yelpCamp"
     }
 });
+
+var info = document.getElementById("helpfulInfo");
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 15);
+}
+setTimeout(function(){
+    fade(info);
+},5000);
+
