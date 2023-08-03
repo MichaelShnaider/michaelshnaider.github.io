09b6a97c9ca5279ade0b43f083a4f811d73d3cd0
Michael Shnaider <mshnaider@contextlogic.com>
2023-08-03 19:45:47 -0400

    Helpful

--- index.js
+++ index.js
@@ -9,55 +9,54 @@ const containers = [
     "devcertified", 
     "cruizor", 
     "aboutMe"
-  ];
-  
-  let mainCircle = document.querySelector(".mainCircle");
-  let currentlyActive = "aboutMe";
-  
-  function hideContainers() {
+];
+
+let mainCircle = document.querySelector(".mainCircle");
+let currentlyActive = "aboutMe";
+
+function hideContainers() {
     containers.forEach(container => {
-      if (container !== currentlyActive) {
-        document.querySelector(`.${container}Container`).style.display = "none";
-      } else {
-        document.querySelector(`.${container}Container`).style.display = "block";
-      }
+        if (container !== currentlyActive) {
+            document.querySelector(`.${container}Container`).style.display = "none";
+        } else {
+            document.querySelector(`.${container}Container`).style.display = "block";
+        }
     });
-  }
-  
-  function changeActive(newActive) {
+}
+
+function changeActive(newActive) {
     if (currentlyActive !== newActive) {
-      mainCircle.classList.add(`${newActive}Circle`);
-      mainCircle.classList.remove(`${currentlyActive}Circle`);
-      document.querySelector(`.${currentlyActive}Container`).style.display = "none";
-      document.querySelector(`.${newActive}Container`).style.display = "block";
-      currentlyActive = newActive;
+        mainCircle.classList.add(`${newActive}Circle`);
+        mainCircle.classList.remove(`${currentlyActive}Circle`);
+        document.querySelector(`.${currentlyActive}Container`).style.display = "none";
+        document.querySelector(`.${newActive}Container`).style.display = "block";
+        currentlyActive = newActive;
     }
-  }
-  
-  hideContainers();
-  
-  containers.forEach(container => {
+}
+
+hideContainers();
+
+containers.forEach(container => {
     document.getElementById(`${container}Small`).addEventListener("click", function() {
-      changeActive(container);
+        changeActive(container);
     });
-  });
-  
-  const info = document.getElementById("helpfulInfo");
-  
-  function fade(element) {
+});
+
+const info = document.getElementById("helpfulInfo");
+
+function fade(element) {
     let op = 1;
     const timer = setInterval(function () {
-      if (op <= 0.1){
-        clearInterval(timer);
-        element.style.display = 'none';
-      }
-      element.style.opacity = op;
-      element.style.filter = `alpha(opacity=${op * 100})`;
-      op -= op * 0.1;
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = `alpha(opacity=${op * 100})`;
+        op -= op * 0.1;
     }, 15);
-  }
-  
-  setTimeout(function(){
+}
+
+setTimeout(function(){
     fade(info);
-  }, 10000);
-  
+}, 10000);
